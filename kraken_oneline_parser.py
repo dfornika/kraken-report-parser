803a68f3b8022a741c6a0a5014c4e5e1402f0175
Dan Fornika <dfornika@gmail.com>
2017-09-19 15:07:29 -0700

    Added total_reads to output, fixed counting error

--- kraken_oneline_parser.py
+++ kraken_oneline_parser.py
@@ -11,13 +11,7 @@ filenames = sys.argv[4:]
 rank_codes = {'U', 'D', 'K', 'P', 'C', 'O', 'F', 'G', 'S'}
 assert selected_rank_code in rank_codes, "Rank must be one of [(U)nclassified, (D)omain, (K)ingdom, (P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, or (S)pecies.]"
 
-total_reads = 0
-unclassified_reads = 0
-total_reads_reported = 0
-other_reads = 0
-results = []
-
-header = "sample\tpercent_unclassified\tnum_reads_unclassified\tpercent_other\tnum_reads_other"
+header = "sample\ttotal_reads\tpercent_unclassified\tnum_reads_unclassified\tpercent_other\tnum_reads_other"
 
 for i in range(max_columns):
     header += "\tpercent_reads_in_clade" + str(i + 1) + "\tnumber_reads_in_clade" + str(i + 1) + "\tncbi_taxonomy_id" + str(i + 1) + "\tclade_name" + str(i + 1)
@@ -25,6 +19,11 @@ for i in range(max_columns):
 print header
 
 for filename in filenames:
+    total_reads = 0
+    unclassified_reads = 0
+    total_reads_reported = 0
+    other_reads = 0
+    results = []
     f = open(filename, 'r')
     for line in f:
         line = line.rstrip('\n').split("\t")
@@ -47,14 +46,14 @@ for filename in filenames:
 
         if rank_code == selected_rank_code and percent_reads_in_clade >= percent_threshold:
             results.append({'percent_reads_in_clade': percent_reads_in_clade, 'number_reads_in_clade': number_reads_clade, 'ncbi_taxonomy_id': ncbi_taxonomy_id, 'clade_name': clade_name})
-            total_reads_reported += number_reads_clade
 
-    top_results = sorted(results, key=lambda k: k['percent_reads_in_clade'])[:max_columns]
-    
+    top_results = sorted(results, key=lambda k: k['percent_reads_in_clade'], reverse=True)[:max_columns]
+    total_reads_reported = sum(result['number_reads_in_clade'] for result in top_results)
     total_reads_reported += unclassified_reads
     other_reads = total_reads - total_reads_reported
     
     output =  filename + '\t' + \
+        str(total_reads) + '\t' + \
         str('%.3f' % (unclassified_reads / float(total_reads) * 100)) + '\t' + str(unclassified_reads) + '\t' +\
         str('%.3f' % (other_reads / float(total_reads) * 100)) + '\t' + str(other_reads)
 
