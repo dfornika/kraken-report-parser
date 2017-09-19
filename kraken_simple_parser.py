59bee0ba9debdab45497250559f6ebfd68b1218f
Dan Fornika <dfornika@gmail.com>
2017-09-19 09:28:36 -0700

    Updated simple parser

--- kraken_simple_parser.py
+++ kraken_simple_parser.py
@@ -3,12 +3,16 @@
 import sys
 
 f = open(sys.argv[1], 'r')
-rank = sys.argv[2].upper()
+selected_rank_code = sys.argv[2].upper()
 percent_threshold = float(sys.argv[3])
 
-assert rank in {'U', 'D', 'K', 'P', 'C', 'O', 'F', 'G', 'S'}, "Rank must be one of [(U)nclassified, (D)omain, (K)ingdom, (P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, or (S)pecies.]"
+rank_codes = {'U', 'D', 'K', 'P', 'C', 'O', 'F', 'G', 'S', '-'}
+assert rank_code in rank_codes, "Rank must be one of [(U)nclassified, (D)omain, (K)ingdom, (P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, or (S)pecies.]"
 
 total_reads = 0
+unclassified_reads = 0
+total_reads_reported = 0
+other_reads = 0
 
 header = "percent_reads_in_clade\tnum_reads_in_clade\ttaxid\tname"
 
@@ -23,20 +27,22 @@ for line in f:
     ncbi_taxonomy_id = line[4]
     clade_name = line[5].lstrip(' ')
 
-    total_reads += number_reads_taxon
-
     if clade_name == "unclassified":
+        unclassified_reads = number_reads_clade
         total_reads += number_reads_clade
+        continue
     elif clade_name == "root":
         total_reads += number_reads_clade
+        continue
 
     percent_reads_in_clade = number_reads_clade / float(total_reads) * 100
-    
-    if rank_code != rank:
-        continue
-    
-    if percent_reads_in_clade > percent_threshold:
+
+    if rank_code == selected_rank_code and percent_reads_in_clade >= percent_threshold:
         print str('%.3f' % (percent_reads_in_clade)) + '\t' + str(number_reads_clade) + '\t' + ncbi_taxonomy_id + '\t' + clade_name
+        total_reads_reported += number_reads_clade
 
-    
 
+print str('%.3f' % (unclassified_reads / float(total_reads) * 100)) + '\t' + str(unclassified_reads) + '\t\t' + 'unclassified'
+total_reads_reported += unclassified_reads
+other_reads = total_reads - total_reads_reported
+print str('%.3f' % (other_reads / float(total_reads) * 100)) + '\t' + str(other_reads) + '\t\t' + 'other'
